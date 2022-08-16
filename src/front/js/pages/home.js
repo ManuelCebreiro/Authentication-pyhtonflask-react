e1d18b7c10325d1ebbb62fac0a5254b399edf10d
ManuelCebreiro <cebreirom@gmail.com>
2022-08-16 19:28:24 +0000

    dandole

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,36 +1,59 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
+	const { email, setEmail } = useState("");
+	const { password, setPassword } = useState("")
 
+	const handleClick = () => {
+
+		const opts = {
+			method: 'POST',
+			body: JSON.stringify({
+				email: email,
+				password: password
+			})
+		}
+		fetch("https://3001-manuelcebre-authenticat-j021ujkzxq1.ws-eu61.gitpod.io/api/token", opts)
+			.then(resp => {
+				if (resp.status === 200) return resp.json();
+				else alert("Ha habido un error");
+			})
+			.then()
+			.catch(error => {
+				console.error("Hubo un error", error);
+			})
+
+
+	}
 	return (
-		<div>
-			<div id="login" className="gradient-custom-3">
+		<div className="gradient-custom-3">
+			<div id="login">
 
-				<h3 class="text-center text-white pt-5" >Login form</h3>
-				<div className="container">
+				<h3 class="text-center text-white pt-5 mt-0 mb-4" >Login form</h3>
+				<div className="container vh-100">
 					<div id="login-row" class="row justify-content-center align-items-center">
-						<div id="login-column" class="col-md-6">
+						<div id="login-column" class="col-md-6 bg-white">
 							<div id="login-box" class="col-md-12">
 								<form id="login-form" class="form" action="" method="post">
 									<h3 class="text-center text-info">Login</h3>
 									<div class="form-group">
 										<label for="username" class="text-info">Username:</label><br />
-										<input type="text" name="username" id="username" class="form-control" />
+										<input type="text" name="username" id="username" class="form-control" value={email} onChange={(e) => setEmail(e.target.value)} />
 									</div>
 									<div class="form-group">
 										<label for="password" class="text-info">Password:</label><br />
-										<input type="text" name="password" id="password" class="form-control" />
+										<input type="text" name="password" class="form-control" value={password} onChange={(e) => setPassword(e.target.value)} />
 									</div>
-									<div class="form-group">
+									{/* <div class="form-group">
 										<label for="remember-me" class="text-info"><span>Remember me</span> <span><input id="remember-me" name="remember-me" type="checkbox" /></span></label><br />
 										<input type="submit" name="submit" class="btn btn-info btn-md" value="submit" />
-									</div>
+									</div> */}
 									<div id="register-link" class="text-right">
-										<a href="#" class="text-info">Registrarse</a>
+										<button href="#" class="text-info" onClick={handleClick}>Registrarse</button>
 									</div>
 
 								</form>
